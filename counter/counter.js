dd3f4199685250b1fcc82b55bfc65550c1024d3e
stenders <stenders@163.com>
2013-10-15 14:50:13 +0800

    counter update

--- counter/counter.js
+++ counter/counter.js
@@ -1,4 +1,5 @@
 // counter plugin
+// usage: see below
 
 var $ = $ || {}
 $.counter = function(config){
@@ -11,12 +12,9 @@ $.counter = function(config){
   this.to = +config.to
   
   this.day = config.day
-  this.hours1 = config.hours1
-  this.hours2 = config.hours2
-  this.minutes1 = config.minutes1
-  this.minutes2 = config.minutes2
-  this.seconds1 = config.seconds1
-  this.seconds2 = config.seconds2
+  this.hours = config.hours
+  this.minutes = config.minutes
+  this.seconds = config.seconds
 
   this.finished = config.finished || noop
   this.beforeStart = config.beforeStart || noop
@@ -28,57 +26,65 @@ $.counter.prototype = {
   constructor : $.counter,
 
   init : function(that){
-    var now = +new Date
+    var now = new Date
+    var time = +now
 
-    if(now >= that.to){ // finished
-      return that.finished.call(that)
-    } else if(now < that.from){ // not start yet
-      that.beforeStart.call(that)
-    } else {  // in time
-      that.set( that.calc( that.to - now ) ).progress.call(that)
+    if(time > that.to){ // finished
+      return that.set().finished.call(that)
     }
+
+    if(time < that.from){ // not start yet
+      return that.set().beforeStart.call(that)
+    }
+    // in time
+    var left = that.to - time
+    that.set( that.calc( left ) )
     setTimeout(function(){
       that.init(that)
+      that.progress.call(that)
     },1000)
   },
   set : function(o){
-    o.hours = this.fix(o.fullHours)
-    o.minutes = this.fix(o.minutes)
-    o.seconds = this.fix(o.seconds)
-
-    this.hours1.innerText = o.hours[0]
-    this.hours2.innerText = o.hours[1]
-    this.minutes1.innerText = o.minutes[0]
-    this.minutes2.innerText = o.minutes[1]
-    this.seconds1.innerText = o.seconds[0]
-    this.seconds2.innerText = o.seconds[1]
+    this.day.text(o ? this.fix(o.day) : '00')
+    this.hours.text(o ? this.fix(o.hours) : '00')
+    this.minutes.text(o ? this.fix(o.minutes) : '00')
+    this.seconds.text(o ? this.fix(o.seconds) : '00')
     return this
   },
   fix : function(n){
-    return (n < 10 ? '0' : '') + n
+    return n < 10 ? '0' + n : n
   },
   calc : function(t){
     return {
       day : Math.floor( t / (1000*60*60*24) ),
-      fullHours: Math.floor( t / (1000*60*60) ),
       hours : Math.floor( t / (1000*60*60)%24 ),
       minutes : Math.floor( t / (1000*60)%60 ),
       seconds : Math.floor( t / (1000)%60 )
     };
   }
 }
-window.onload = function(){
-  var $id = function(id){
-    return document.getElementById(id)
-  }
-  $.counter({
-    from    : new Date(2013, 9, 22),
-    to      : new Date(2013, 9, 24),
-    hours1  : $id('jos-hours-1'),
-    hours2  : $id('jos-hours-2'),
-    minutes1: $id('jos-minutes-1'),
-    minutes2: $id('jos-minutes-2'),
-    seconds1: $id('jos-seconds-1'),
-    seconds2: $id('jos-seconds-2')
+
+// example
+
+$(document).ready(function(){
+  var aa = $.counter({
+    day: $('.day'),               // day selector
+    hours: $('.hour'),            // hour selector
+    minutes: $('.minute'),        // minute selector 
+    seconds: $('.second'),        // seconds selector
+    from: new Date(2013, 2, 22),  // start date
+    to: new Date(2013, 9, 24, 12, 18),    // deadline
+    beforeStart: function(){      // before-start callback
+      alert('not start yet')
+      console.log(this)
+    },
+    progress : function(){        // progress callback
+      // alert('running!')
+      console.log(this)
+    },
+    finished: function(){         // out-of-date callback
+      alert('finished')
+      console.log(this)
+    }
   })
-}
+})
