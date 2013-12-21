c65c10d808d2fed68d53388d471796c20c110e7b
stenders <stenders@163.com>
2013-12-21 14:39:45 +0800

    update placeholder.js

--- placeholder/jquery.placeholder.js
+++ placeholder/jquery.placeholder.js
@@ -5,16 +5,18 @@
  * Date : 2012.10.30 18:40
  */
 ;(function($, document){
-	$.fn.placeholder = function(){
+	$.fn.placeholder = function(options){
 		return 'placeholder' in document.createElement('input') ? this : this.each(function(){
 			var that = this,
 			focusFunc = function(){
-				if(that.value == that.defaultValue)
-					that.value = '';
+				if(that.value != that.defaultValue) return
+				that.value = '';
+				that.style.color = options.color || '#000';
 			},
 			blurFunc = function(){
-				if(that.value == '')
-					that.value = that.defaultValue;
+				if(that.value !== '') return
+				that.value = that.defaultValue;
+				that.style.color = '#999';
 			};
 			that.defaultValue = that.value = that.getAttribute('placeholder');
 			if(that.attachEvent){
@@ -24,9 +26,10 @@
 				that.addEventListener('focus', focusFunc, false);
 				that.addEventListener('blur', blurFunc, false);
 			}
+			that.style.color = '#999';
 		});
 	};
 	$(function(){
-		$('[placeholder]').placeholder();
+		$('[placeholder]').placeholder({color: '#000'});
 	});
 })(jQuery, document);
